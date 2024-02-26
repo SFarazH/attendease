1be41c2f99f20e90a569a3d2df58e88bf928bd39
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-02-26 11:00:55 +0530

    update env

--- attendease/settings.py
+++ attendease/settings.py
@@ -13,13 +13,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from django.conf.urls.static import static
 from django.conf import settings
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-426c#40*2rxxgr5lfw_ces57pm1#jrv%7!gw8_ojd6vb98%$rq'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
