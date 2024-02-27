d237e76fa3dccec55fe9b4eba0ccf9405baeb183
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-02-27 15:39:00 +0530

    rem static

--- static/js/error.js
+++
@@ -1,9 +0,0 @@
-var form = document.getElementById("inputForm");
-var spinner = document.getElementById("spinnerDiv");
-var overlay = document.getElementById("overlay");
-
-form.addEventListener("submit", () => {
-  spinner.classList.add("spinner-border");
-  overlay.classList.add("overlay");
-  console.log("ok");
-});
